76e0a025ed569deabc4303fadd37a13071ad7116
Jelani2022 <jtwash2@gmail.com>
2022-06-13 10:32:18 -0400

    ex 3&4

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 console.log("Hello World!\n==========\n");
 
+function array.prototype.reduce(){
+
+}
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+let title;
+let pages;
+let pageCount;
+counsole.log(`${title} ${pages} ${reacCount}`)
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
 
+// Exercise 3
+
+
+
+
+// Exercise 4
